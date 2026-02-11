3e1c4d2a865bbd146097043457630d256a792cb2
lynnswap <65545348+lynnswap@users.noreply.github.com>
2026-02-11 14:46:28 +0900

    Bump MachOSwiftSection revision for dependency indexing timing

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/lynnswap/MachOSwiftSection.git",
-            revision: "61584dcd738df90c348afe2f0aec57403c83596c"
+            revision: "5d032fd36443dd060d149c79e1beb945b0f4f2f8"
         ),
         .package(
             url: "https://github.com/lynnswap/swift-objc-dump.git",
